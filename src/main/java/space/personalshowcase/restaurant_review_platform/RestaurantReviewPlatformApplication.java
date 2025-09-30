2c11504f146e37f31006966fd38718054fb2338d
adityamehta242 <adityamehta242@gmail.com>
2025-10-01 00:39:16 +0530

    complete project setup done

---
+++ src/main/java/space/personalshowcase/restaurant_review_platform/RestaurantReviewPlatformApplication.java
@@ -0,0 +1,13 @@
+package space.personalshowcase.restaurant_review_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReviewPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReviewPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/space/personalshowcase/restaurant_review_platform/RestaurantReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package space.personalshowcase.restaurant_review_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
