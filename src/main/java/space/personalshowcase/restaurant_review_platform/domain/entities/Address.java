dcd02bd58ae726baa87fcabc6a6b3845e5f6bcab
adityamehta242 <adityamehta242@gmail.com>
2025-10-03 01:12:30 +0530

    add entity TimeRange & OperatingHours

--- src/main/java/space/personalshowcase/restaurant_review_platform/domain/entities/Address.java
+++ src/main/java/space/personalshowcase/restaurant_review_platform/domain/entities/Address.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.elasticsearch.annotations.Document;
+// import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
