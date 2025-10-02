49a767325cca74a6c4805e7ccfb92f7173c32003
adityamehta242 <adityamehta242@gmail.com>
2025-10-03 01:27:59 +0530

    add entity Photo and Review

---
+++ src/main/java/space/personalshowcase/restaurant_review_platform/domain/entities/Photo.java
@@ -0,0 +1,25 @@
+package space.personalshowcase.restaurant_review_platform.domain.entities;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.elasticsearch.annotations.DateFormat;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Document(indexName = "photo")
+public class Photo {
+
+    @Field(type = FieldType.Keyword)
+    private String url;
+
+    @Field(type = FieldType.Date, format = DateFormat.date_hour_minute_second)
+    private LocalDateTime uploadDate;
+}
