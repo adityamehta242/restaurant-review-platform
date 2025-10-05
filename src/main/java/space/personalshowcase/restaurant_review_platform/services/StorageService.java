b46e708cda3e2bac65fa707e560fe5bf31012189
adityamehta242 <adityamehta242@gmail.com>
2025-10-06 00:18:58 +0530

    make file storage service

---
+++ src/main/java/space/personalshowcase/restaurant_review_platform/services/StorageService.java
@@ -0,0 +1,11 @@
+package space.personalshowcase.restaurant_review_platform.services;
+
+import java.util.Optional;
+
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
+    String store(MultipartFile file , String filename);
+    Optional<Resource> loadAsResource(String id);
+}
