12ecd8c487e2b0f2dc86baf5722e856fee8e7fa5
adityamehta242 <adityamehta242@gmail.com>
2025-10-07 00:50:26 +0530

    Implement Exception   - BaseException   - StorageException

---
+++ src/main/java/space/personalshowcase/restaurant_review_platform/exceptions/BaseException.java
@@ -0,0 +1,18 @@
+package space.personalshowcase.restaurant_review_platform.exceptions;
+
+public class BaseException extends RuntimeException {
+    public BaseException(){}
+
+    public BaseException(String message){
+        super(message);
+    }
+
+    public BaseException(String message , Throwable cause)
+    {
+        super(message , cause);
+    }
+
+    public BaseException(Throwable cause){
+        super(cause);
+    }
+}
