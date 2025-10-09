4326b571eba2eb3268caf9fd424614fcc9d2fcef
aditya mehta <adityamehta242@gmail.com>
2025-10-10 00:38:40 +0530

    move SecurityConfig to Config package

--- src/main/java/space/personalshowcase/restaurant_review_platform/config/SecurityConfig.java
+++ src/main/java/space/personalshowcase/restaurant_review_platform/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package space.personalshowcase.restaurant_review_platform.domain.entities;
+package space.personalshowcase.restaurant_review_platform.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
