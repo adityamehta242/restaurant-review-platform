a2ff6452afe03ff6e92b1e5dde788a06810abc74
aditya mehta <adityamehta242@gmail.com>
2025-10-25 00:17:34 +0530

    feat: add RestaurantSummaryDto and map with Restaurant

--- src/main/java/space/personalshowcase/restaurant_review_platform/mappers/RestaurantMapper.java
+++ src/main/java/space/personalshowcase/restaurant_review_platform/mappers/RestaurantMapper.java
@@ -2,13 +2,18 @@ package space.personalshowcase.restaurant_review_platform.mappers;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.ReportingPolicy;
 import org.springframework.data.elasticsearch.core.geo.GeoPoint;
 import space.personalshowcase.restaurant_review_platform.domain.RestaurantCreateUpdateRequest;
 import space.personalshowcase.restaurant_review_platform.domain.dtos.GeoPointDto;
 import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantCreateUpdateRequestDto;
 import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantDto;
+import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantSummaryDto;
 import space.personalshowcase.restaurant_review_platform.domain.entities.Restaurant;
+import space.personalshowcase.restaurant_review_platform.domain.entities.Review;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring" , unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface RestaurantMapper {
@@ -16,6 +21,14 @@ public interface RestaurantMapper {
     RestaurantCreateUpdateRequest toRestaurantCreateUpdateRequest(RestaurantCreateUpdateRequestDto dto);
 
     RestaurantDto toRestaurantDto(Restaurant restaurant);
+
+    @Mapping(source = "reviews" , target = "totalReviews" , qualifiedByName = "populateTotalReviews")
+    RestaurantSummaryDto toRestaurantSummaryDto(Restaurant restaurant);
+
+    @Named("populateTotalReviews")
+    private Integer populateTotalReviews(List<Review> reviews){
+        return reviews.size();
+    }
 
     @Mapping(target = "latitude",expression = "java(geoPoint.getLat())")
     @Mapping(target = "longitude",expression = "java(geoPoint.getLon())")
