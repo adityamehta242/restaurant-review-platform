917d44c0a5046f0392868540dd4d07274a1b483d
aditya mehta <adityamehta242@gmail.com>
2025-10-26 15:06:15 +0530

    feat: implement searchRestaurants endpoint

--- src/main/java/space/personalshowcase/restaurant_review_platform/controllers/RestaurantController.java
+++ src/main/java/space/personalshowcase/restaurant_review_platform/controllers/RestaurantController.java
@@ -2,13 +2,14 @@ package space.personalshowcase.restaurant_review_platform.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import space.personalshowcase.restaurant_review_platform.domain.RestaurantCreateUpdateRequest;
 import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantCreateUpdateRequestDto;
 import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantDto;
+import space.personalshowcase.restaurant_review_platform.domain.dtos.RestaurantSummaryDto;
 import space.personalshowcase.restaurant_review_platform.domain.entities.Restaurant;
 import space.personalshowcase.restaurant_review_platform.mappers.RestaurantMapper;
 import space.personalshowcase.restaurant_review_platform.services.RestaurantService;
@@ -31,6 +32,23 @@ public class RestaurantController {
 
         RestaurantDto createRestaurantDto = restaurantMapper.toRestaurantDto(restaurant);
         return ResponseEntity.ok(createRestaurantDto);
+    }
+
+    @GetMapping
+    public Page<RestaurantSummaryDto> searchRestaurants(
+            @RequestParam(required = false) String q,
+            @RequestParam(required = false) Float minRating,
+            @RequestParam(required = false) Float latitude,
+            @RequestParam(required = false) Float longitude,
+            @RequestParam(required = false) Float radius,
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(defaultValue = "20") int size
+    ){
+        Page<Restaurant> searchedRestaurants = restaurantService.searchRestaurants(
+                q , minRating , latitude , longitude , radius , PageRequest.of(page - 1 , size)
+        );
+
+        return searchedRestaurants.map(restaurantMapper::toRestaurantSummaryDto);
     }
 
 }
