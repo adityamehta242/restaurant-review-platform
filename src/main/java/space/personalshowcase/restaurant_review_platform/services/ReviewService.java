c49a0b0b3bf04b42ec400cf0b57b7055509fe37a
aditya mehta <adityamehta242@gmail.com>
2025-11-01 05:11:06 +0530

    feat: ReviewService

--- src/main/java/space/personalshowcase/restaurant_review_platform/services/ReviewService.java
+++ src/main/java/space/personalshowcase/restaurant_review_platform/services/ReviewService.java
@@ -1,4 +1,9 @@
 package space.personalshowcase.restaurant_review_platform.services;
 
+import space.personalshowcase.restaurant_review_platform.domain.ReviewCreateUpdateRequest;
+import space.personalshowcase.restaurant_review_platform.domain.entities.Review;
+import space.personalshowcase.restaurant_review_platform.domain.entities.User;
+
 public interface ReviewService {
+    Review createReview(User author, String restaurantId, ReviewCreateUpdateRequest review);
 }
